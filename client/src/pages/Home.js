23751f1f5fc1fd30f4556e79099f2a12979c935c
Karen Seunsom <karenseuns@gmail.com>
2021-10-21 13:10:05 -0500

    changed style of menu cards

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Container, Card, Row, Col } from 'react-bootstrap'
 import poundCake from '../images/pound_cake.jpg'
+import halloStraws from '../images/halloween_strawberries.jpg'
+import empanadas from '../images/empanadas.jpg'
+import cake from '../images/flower_cake.jpg'
 
 export default function Home() {
     return (
@@ -22,7 +25,7 @@ export default function Home() {
                     </Col>
                     <Col>
                         <Card className="bg-dark text-white">
-                            <Card.Img src={poundCake} alt="Card image" />
+                            <Card.Img src={halloStraws} alt="Card image" />
                             <Card.ImgOverlay>
                                 <Card.Title>Pound Cake</Card.Title>
                                 <Card.Text>
@@ -33,7 +36,7 @@ export default function Home() {
                     </Col>
                     <Col>
                         <Card className="bg-dark text-white">
-                            <Card.Img src={poundCake} alt="Card image" />
+                            <Card.Img src={cake} alt="Card image" />
                             <Card.ImgOverlay>
                                 <Card.Title>Pound Cake</Card.Title>
                                 <Card.Text>
@@ -44,7 +47,7 @@ export default function Home() {
                     </Col>
                     <Col>
                         <Card className="bg-dark text-white">
-                            <Card.Img src={poundCake} alt="Card image" />
+                            <Card.Img src={empanadas} alt="Card image" />
                             <Card.ImgOverlay>
                                 <Card.Title>Pound Cake</Card.Title>
                                 <Card.Text>
