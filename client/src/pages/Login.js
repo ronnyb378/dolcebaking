f1d406059d73d82a452e4a0193b40074a0058136
Karen Seunsom <karenseuns@gmail.com>
2021-10-28 19:02:56 -0500

    login/signup state added to login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,68 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Form, Row, Col, Button, FloatingLabel } from 'react-bootstrap'
 
 export default function Login() {
+
+    const [login, setLogin] = useState(true)
+
+    function toggleForm() {
+        setLogin(!login)
+    }
+
     return (
         <div>
             <Container className="pt-4 login-form" fluid>
-                <h2>Log in </h2>
                 <Row className="justify-content-center">
+                    {login ? (
                     <Col >
-                        {/* <h2>Login</h2> */}
-                        <Form>
-                            <FloatingLabel
-                                controlId="floatingInput"
-                                label="Email address"
-                                className="mb-3"
-                            >
-                                <Form.Control size="lg" type="email" placeholder="name@example.com" />
-                            </FloatingLabel>
-                            <FloatingLabel controlId="floatingPassword" label="Password">
-                                <Form.Control size="lg" type="password" placeholder="Password" />
-                            </FloatingLabel>
-                            <Button size="lg" type="submit">Sign in</Button>
-                        </Form>
-                        <Button className="accountBtn" size="lg">Create an Account</Button>
+                        <h2>Log in</h2>
+                            <Form>
+                                <FloatingLabel
+                                    controlId="floatingInput"
+                                    label="Email address"
+                                    className="mb-3"
+                                >
+                                    <Form.Control size="lg" type="email" placeholder="name@example.com" />
+                                </FloatingLabel>
+                                <FloatingLabel controlId="floatingPassword" label="Password">
+                                    <Form.Control size="lg" type="password" placeholder="Password" />
+                                </FloatingLabel>
+                                <Form.Check
+                                    type="checkbox"
+                                    label="Keep me signed in"
+                                />
+                                <Button size="lg" type="submit">Sign in</Button>
+                                <Button className="guestBtn" size="lg">
+                                Sign in as Guest
+                                </Button>
+                                <Button onClick={() => toggleForm()} className="accountBtn" size="lg">Create an Account</Button>
+                            </Form>
                     </Col>
-                    {/* <Col>
-                        <Form>
+                    ) : (
+                    <Col>
+                        <h2>Sign Up</h2>
+                        <Form className="signUpForm">
                             <Row className="mb-3">
                                 <Form.Group as={Col} controlId="firstName">
                                     <Form.Label>First Name</Form.Label>
                                     <Form.Control size="lg" placeholder="John" />
                                 </Form.Group>
-
                                 <Form.Group as={Col} controlId="lastName">
                                     <Form.Label>Last Name</Form.Label>
                                     <Form.Control size="lg" placeholder="Doe" />
                                 </Form.Group>
                             </Row>
-
                             <Form.Group className="mb-3" controlId="signUpEmail">
                                 <Form.Label>Email address</Form.Label>
-                                <Form.Control size="lg" type="email" placeholder="Enter email" />
+                                <Form.Control size="lg" type="email" placeholder="example@example.com" />
                             </Form.Group>
-
                             <Form.Group className="mb-3" controlId="number">
                                 <Form.Label>Phone Number</Form.Label>
                                 <Form.Control size="lg" type="email" placeholder="(123)456-7890" />
                             </Form.Group>
-
                             <Form.Group className="mb-3" controlId="signUpPassword">
                                 <Form.Label>Password</Form.Label>
                                 <Form.Control size="lg" type="password" placeholder="Password" />
                             </Form.Group>
-
                             <Form.Group className="mb-3" controlId="confirmPassword">
                                 <Form.Label>Confirm Password</Form.Label>
                                 <Form.Control size="lg" type="password" placeholder="Confirm Password" />
                             </Form.Group>
-
                             <Button variant="primary" type="submit" size="lg">
-                                Submit
+                                Create Account
                             </Button>
+                            <p onClick={() => toggleForm()}>Already have an account? Sign in here.</p>
                         </Form>
-                    </Col> */}
+                    </Col>
+                    )}
                 </Row>
             </Container>
         </div>
