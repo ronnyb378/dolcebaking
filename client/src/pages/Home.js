5264981e17bed89b835eceb6bb96931f1432c729
Karen Seunsom <karenseuns@gmail.com>
2021-11-01 16:48:41 -0500

    bare bones modal set up

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import { Container, Card, Row, Col } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Container, Card, Row, Col, Button } from 'react-bootstrap'
 import poundCake from '../images/pound_cake.jpg'
 import halloStraws from '../images/halloween_strawberries.jpg'
 import empanadas from '../images/empanadas.jpg'
 import cake from '../images/flower_cake.jpg'
+import ItemPopUp from '../components/ItemPopUp'
 
 export default function Home() {
+    const [modalShow, setModalShow] = useState(false)
+
     return (
         <div>
             <Container className="pt-2 pb-4 menu" fluid>
@@ -14,11 +17,18 @@ export default function Home() {
                 <Row xs={1} md={2} lg={2} xl={4} className="g-4">
                     <Col>
                         <Card className="bg-dark text-white">
-                            <Card.Img src={poundCake} alt="Card image"/>
+                            <Card.Img src={poundCake} alt="Card image" />
                             <Card.ImgOverlay>
                                 <Card.Title>Pound Cake</Card.Title>
                                 <Card.Text>
-                                    Order
+                                    <Button variant="primary" onClick={() => setModalShow(true)}>
+                                        Launch vertically centered modal
+                                    </Button>
+
+                                    <ItemPopUp
+                                        show={modalShow}
+                                        onHide={() => setModalShow(false)}
+                                    />
                                 </Card.Text>
                             </Card.ImgOverlay>
                         </Card>
