3b828ef58d721e2651877c031e1eb5a822002426
Karen Seunsom <karenseuns@gmail.com>
2021-12-02 14:54:16 -0600

    css to home page, menu page, and menu cards. About us component also added

---
+++ client/src/components/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+export default function AboutUs() {
+    return (
+        <div>
+            <Container className="pb-2">
+                <h2>Our Story</h2>
+                <p>Cupcake ipsum dolor sit amet halvah bear claw. Dessert ice cream sesame snaps fruitcake gingerbread bear claw macaroon dessert. Jujubes cookie chocolate bar biscuit lemon drops caramels wafer. Macaroon cheesecake chocolate gummi bears icing marshmallow dessert. Danish topping bear claw cookie cookie. Cupcake topping pastry donut pudding caramels wafer sweet gingerbread. Caramels tart apple pie ice cream liquorice pudding bear claw chocolate wafer. Topping apple pie tootsie roll bear claw apple pie brownie. Sugar plum croissant sesame snaps jelly beans powder danish toffee halvah. Tootsie roll sweet roll jujubes powder chocolate cake sweet roll.</p>
+            </Container>
+        </div>
+    )
+}
