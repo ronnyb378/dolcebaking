ec8ed0cd7bd9dbe0f711c18bb23b3d0bfc176516
Karen Seunsom <karenseuns@gmail.com>
2021-12-01 23:06:06 -0600

    handled sending form data on login page with post requests

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,7 +3,71 @@ import { Container, Form, Row, Col, Button, FloatingLabel } from 'react-bootstra
 
 export default function Login() {
 
+    // form state
     const [login, setLogin] = useState(false)
+
+    // login states
+    const [ loginUsername, setLoginUsername ] = useState('');
+    const [ loginPassword, setLoginPassword ] = useState('');
+
+    // sign up states
+    const [ signupUsername, setSignupUsername ] = useState('');
+    const [ signupEmail, setSignupEmail ] = useState('');
+    const [ firstName, setFirstName ] = useState('');
+    const [ lastName, setLastName ] = useState('');
+    const [ phoneNumber, setPhoneNumber ] = useState('');
+    const [ signupPassword, setSignupPassword ] = useState('');
+    const [ signupConfirmPassword, setSignupConfirmPassword ] = useState('')
+
+    const handleLoginSubmit = (e) => {
+        e.preventDefault()
+        console.log('fetch')
+        fetch('/api/v1/users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: loginUsername,
+                password: loginPassword,
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                console.log(data.success)
+            }
+        })
+    }
+
+    const handleSignupSubmit = (e) => {
+        e.preventDefault()
+        fetch('/api/v1/users/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: signupUsername,
+                email: signupEmail,
+                first_name: firstName,
+                last_name: lastName,
+                phone_number: phoneNumber,
+                password: signupPassword,
+                confirmed_password: signupConfirmPassword
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            if (data.success) {
+                console.log(data.success)
+            } else {
+                console.log(data.error)
+            }
+        })
+    }
 
     const handleFormChange = (e) => {
         e.preventDefault()
@@ -18,32 +82,43 @@ export default function Login() {
                     {login === true ? (
                         <Col>
                             <h2>Sign Up</h2>
-                            <Form className="signUpForm">
+                            <Form className="signUpForm" onSubmit={handleSignupSubmit}>
                                 <Row className="mb-3">
                                     <Form.Group as={Col} controlId="firstName">
                                         <Form.Label>First Name</Form.Label>
-                                        <Form.Control size="lg" placeholder="John" />
+                                        <Form.Control size="lg" placeholder="John"
+                                        value={firstName} onChange={(e) => setFirstName(e.target.value)} />
                                     </Form.Group>
                                     <Form.Group as={Col} controlId="lastName">
                                         <Form.Label>Last Name</Form.Label>
-                                        <Form.Control size="lg" placeholder="Doe" />
+                                        <Form.Control size="lg" placeholder="Doe"
+                                        value={lastName} onChange={(e) => setLastName(e.target.value)}/>
                                     </Form.Group>
                                 </Row>
                                 <Form.Group className="mb-3" controlId="signUpEmail">
                                     <Form.Label>Email address</Form.Label>
-                                    <Form.Control size="lg" type="email" placeholder="example@example.com" />
+                                    <Form.Control size="lg" type="email" placeholder="example@example.com"
+                                    value={signupEmail} onChange={(e) => setSignupEmail(e.target.value)} />
                                 </Form.Group>
+                                <Form.Group as={Col} controlId="username">
+                                        <Form.Label>Username</Form.Label>
+                                        <Form.Control size="lg" placeholder="BunnieCakes123"
+                                        value={signupUsername} onChange={(e) => setSignupUsername(e.target.value)} />
+                                    </Form.Group>
                                 <Form.Group className="mb-3" controlId="number">
                                     <Form.Label>Phone Number</Form.Label>
-                                    <Form.Control size="lg" type="email" placeholder="(123)456-7890" />
+                                    <Form.Control size="lg" placeholder="(123)456-7890"
+                                    value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
                                 </Form.Group>
                                 <Form.Group className="mb-3" controlId="signUpPassword">
                                     <Form.Label>Password</Form.Label>
-                                    <Form.Control size="lg" type="password" placeholder="Password" />
+                                    <Form.Control size="lg" type="password" placeholder="Password"
+                                    value={signupPassword} onChange={(e) => setSignupPassword(e.target.value)} />
                                 </Form.Group>
                                 <Form.Group className="mb-3" controlId="confirmPassword">
                                     <Form.Label>Confirm Password</Form.Label>
-                                    <Form.Control size="lg" type="password" placeholder="Confirm Password" />
+                                    <Form.Control size="lg" type="password" placeholder="Confirm Password"
+                                    value={signupConfirmPassword} onChange={(e) => setSignupConfirmPassword(e.target.value)} />
                                 </Form.Group>
                                 <Button variant="primary" type="submit" size="lg">
                                     Create Account
@@ -56,16 +131,20 @@ export default function Login() {
 
                         <Col >
                             <h2>Log in</h2>
-                            <Form>
+                            <Form onSubmit={handleLoginSubmit}>
                                 <FloatingLabel
                                     controlId="floatingInput"
                                     label="Email address"
                                     className="mb-3"
                                 >
-                                    <Form.Control size="lg" type="email" placeholder="name@example.com" />
+                                    <Form.Control size="lg" type="email" placeholder="name@example.com" 
+                                    value={loginUsername}
+                                    onChange={e => setLoginUsername(e.target.value)}/>
                                 </FloatingLabel>
                                 <FloatingLabel controlId="floatingPassword" label="Password">
-                                    <Form.Control size="lg" type="password" placeholder="Password" />
+                                    <Form.Control size="lg" type="password" placeholder="Password" 
+                                    value={loginPassword} onChange={e => setLoginPassword(e.target.value)}
+                                    />
                                 </FloatingLabel>
                                 <Form.Check
                                     type="checkbox"
