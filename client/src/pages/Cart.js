1313a2ad9bd04216093bcb2142aa893b170210a6
Ronny Barahona <ronnybarahona@Ronnys-MacBook-Air.local>
2021-12-05 14:22:09 -0600

    stashing changes for updating branch

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Cart() {
     return (
         <div>
-            <h2>Shopping Cart</h2>
+            <h2>Your Cart</h2>
+            <div>
+            <div className="cart-info">Cart Info</div>
+            <div className="cart-summary">Cart Summary</div>
+            </div>
         </div>
     )
 }
