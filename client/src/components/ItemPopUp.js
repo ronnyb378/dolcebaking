f0178882a2817fc38525d378dc1618a7661cb49f
Karen Seunsom <karenseuns@gmail.com>
2021-12-09 19:58:37 -0600

    working on setting error messages if user does not select flavor on item modals

--- client/src/components/ItemPopUp.js
+++ client/src/components/ItemPopUp.js
@@ -9,7 +9,9 @@ import { actionItemDetail } from '../redux/actions/itemDetail'
 export default function ItemPopUp(props) {
     const dispatch = useDispatch()
     const { data } = props
-    const [ detailObj, setDetailObj ] = useState(null)
+    const [detailObj, setDetailObj] = useState(null)
+    const [ clicked, setClicked ] = useState(false)
+    const [ error, setError ] = useState("")
 
     useEffect(() => {
         if (data) {
@@ -28,6 +30,19 @@ export default function ItemPopUp(props) {
         return product
     }
 
+    const validate = () => {
+        let errorMessage = "";
+
+        if (!clicked) {
+            errorMessage = "Please choose a flavor"
+        }
+
+        if (errorMessage) {
+            setError(errorMessage);
+            return false;
+        }
+        return true;
+    }
 
 
     const handleAddToCart = (e, id) => {
@@ -38,67 +53,74 @@ export default function ItemPopUp(props) {
         // product.count += 1;
         // const price = product.price;
         // product.total = price;
-        e.preventDefault();
-        const tempProduct = getItem(id)
-        let product = {...tempProduct}
-        const price = product.price;
-        product.total = price
 
-        dispatch(actionUpdateCart(product))
+        e.preventDefault();
+        const isValid = validate()
+        if (isValid) {
+            const tempProduct = getItem(id)
+            let product = { ...tempProduct }
+            const price = product.price;
+            product.total = price    
+            dispatch(actionUpdateCart(product))
+            setError('')
+        }
+    
     }
 
     const handleRadioButton = id => {
         const product = getItem(id)
         dispatch(actionItemDetail(product))
+        setClicked(true)
     }
 
     if (detailObj) {
-    return (
-        <Modal
-            {...props}
-            // size="lg"
-            dialogClassName="modal-50w"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-        >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    {props.data.name}
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Row>
-                    <Col>
-                        <Image src={props.data.image} fluid rounded />
-                    </Col>
-                    <Col className="d-flex align-items-center item-column-right justify-content-center">
-                        <p>{props.data.description}</p>
-                        {/* form having id and button having form attribute not supported by IE11? */}
-                        <Form id="item-form" onSubmit={(e) => handleAddToCart(e, clickedItem.id)} onChange={(e) => handleRadioButton(e.target.value)}>
-                            {
-                            (props.data.products.length > 1) ? (
-                                props.data.products.map((data) => {
-                                return <Form.Check
-                                        required
-                                        key={data.id}
-                                        value={data.id}
-                                        inline 
-                                        type="radio"
-                                        name="group1"
-                                        id={`default-radio`}
-                                        label={`${data.name}`}
+        return (
+            <Modal
+                {...props}
+                // size="lg"
+                dialogClassName="modal-50w"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        {props.data.name}
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Row>
+                        <Col>
+                            <Image src={props.data.image} fluid rounded />
+                        </Col>
+                        <Col className="d-flex align-items-center item-column-right">
+                            <i>{props.data.description}</i>
+                            {/* form having id and button having form attribute not supported by IE11? */}
+                            <Form noValidate id="item-form" onSubmit={(e) => handleAddToCart(e, clickedItem.id)} onChange={(e) => handleRadioButton(e.target.value)}>
+                                {props.data.products.length > 1 && <h5>Flavors</h5>} { error && <p>{error}</p>}
+                                {(props.data.products.length > 1) ? (
+                                    props.data.products.map(data =>
+                                        <Form.Check
+                                            required
+                                            key={data.id}
+                                            value={data.id}
+                                            inline
+                                            type="radio"
+                                            name="group1"
+                                            id={`default-radio`}
+                                            label={`${data.name}`}
                                         />
-                            })
-                            ) : (null)
-                                } 
-                        </Form>
-                    </Col>
-                </Row>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={props.onHide}>Back To Products</Button>
-                <Button form="item-form" className="cart-btn" type="submit" >Add To Cart</Button>
-            </Modal.Footer>
-        </Modal>
-    )} else { return null }
+                                    )
+                                ) : (null)
+                                }
+                            </Form>
+                        </Col>
+                    </Row>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={props.onHide}>Back To Products</Button>
+                    <Button form="item-form" className="cart-btn" type="submit" >Add To Cart</Button>
+                </Modal.Footer>
+            </Modal>
+        )
+    } else { return null }
 }
