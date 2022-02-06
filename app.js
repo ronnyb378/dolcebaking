fb3a33708db3d404ecfc57ac914dcb6955229131
Karen Seunsom <karenseuns@gmail.com>
2022-02-06 15:47:47 -0600

    move catchall to bottom of app.js file

--- app.js
+++ app.js
@@ -46,4 +46,8 @@ app.use('/', indexRouter);
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/orders', orderRouter);
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 module.exports = app;
